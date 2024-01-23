c63265659154c57991f319e34ce7bdfd13928e1c
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2024-01-23 19:50:02 +0530

    Add files via upload

---
+++ Day 10/Day10Main.py
@@ -0,0 +1,19 @@
+
+
+import Area
+import Dictop
+import findvowels
+
+
+#import Reverse
+
+def main():
+    #Armstrong.username()
+    findvowels.findvowels()
+    Area.area()
+    Dictop.dictop()
+
+if __name__ == "__main__":
+    main()
+
+
