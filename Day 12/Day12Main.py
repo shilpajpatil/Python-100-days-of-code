2d8a00de75be6aac3f0230c52e7eb56151d2ca78
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2024-01-25 19:49:24 +0530

    Add files via upload

---
+++ Day 12/Day12Main.py
@@ -0,0 +1,18 @@
+
+
+#import reverse
+#import Dictop
+
+#import findvowels
+
+import Fizz
+
+def main():
+   # findvowels.findvowels()
+    Fizz.fizz()
+    #Dictop.dictop()
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ Day 12/Fizz.py
@@ -0,0 +1,13 @@
+
+# Fizzbuzz leet code question
+
+def fizz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
