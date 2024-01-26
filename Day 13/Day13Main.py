da2e2559212eb4c5a58a016abd1a07895944f23d
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2024-01-26 23:47:04 +0530

    Add files via upload

---
+++ Day 13/Day13Main.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+#import Pattern1
+import Pattern2
+
+def main():
+
+ #   Pattern1.pattern()
+    Pattern2.pattern()
+
+main()
---
+++ Day 13/Pattern2.py
@@ -0,0 +1,9 @@
+def pattern():
+
+    n= int(input("Enter a number:"))
+
+    for i in range(n):
+        for j in range(n):
+            print("*",end = "")
+
+        print("\r")
