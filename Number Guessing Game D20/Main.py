68683bdb040d360f33fad2a414f3ca9cabd5b425
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2024-03-27 15:21:48 +0530

    Update Main.py

--- Number Guessing Game D20/Main.py
+++ Number Guessing Game D20/Main.py
@@ -1,1 +1,23 @@
 
+
+# Generate a random number in python and gess that randomly generated number :
+
+
+import random
+
+number = random.randint(0,10)
+
+guess = int(input("Enter your guessed number:"))
+#print("randomly generated number:",number)
+
+while number != guess:
+    if guess <number:
+        print("guess is too low")
+        guess = int(input("Enter your guess number again:"))
+    elif (guess > number):
+        print("guess is too high")
+        guess= int(input("Enter your guess number again:"))
+    else:
+        break;
+
+print("your guess is correct")
